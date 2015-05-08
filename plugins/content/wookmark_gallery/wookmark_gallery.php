dad3dbd0ff6ccd33750692b50ebf56e2f03258d2
davidguillemet <davidguillemet@iMac-27-DGUI.local>
2015-05-08 19:57:01 +0200

    Extraction infos IPTC et insertion auto en base
    Extraction infos IPTC (Title + keywords) et insertion auto en base dans
    la table phocagallery

--- plugins/content/wookmark_gallery/wookmark_gallery.php
+++ plugins/content/wookmark_gallery/wookmark_gallery.php
@@ -59,194 +59,284 @@ class plgContentWookmark_Gallery extends JPlugin
 					$document->addCustomTag("<style type='text/css'>
 					.myapp{
 						width: $width;}
-					</style>");
-				}
-				($autoresize_gal==1) ? $autoresize_gal='true' : $autoresize_gal = 'false';
-				if($jsfiles==1)
-				{
-					if($load1==1)
-					{
-						$document->addScript( JURI::root().'plugins/content/wookmark_gallery/wookmark_gallery/tmpl/js/jquery-1.7.2.min.js' );
+						</style>");
 					}
-				}
-				else
-				{
-					if($load1==1)
+					($autoresize_gal==1) ? $autoresize_gal='true' : $autoresize_gal = 'false';
+					if($jsfiles==1)
 					{
-						echo '<script src="'.$base_url.'plugins/content/wookmark_gallery/wookmark_gallery/tmpl/js/jquery-1.7.2.min.js" type="text/javascript"></script>';
+						if($load1==1)
+						{
+							$document->addScript( JURI::root().'plugins/content/wookmark_gallery/wookmark_gallery/tmpl/js/jquery-1.7.2.min.js' );
+						}
 					}
+					else
+					{
+						if($load1==1)
+						{
+							echo '<script src="'.$base_url.'plugins/content/wookmark_gallery/wookmark_gallery/tmpl/js/jquery-1.7.2.min.js" type="text/javascript"></script>';
+						}
+					}
+					$wookmark_css = 1;
 				}
-				$wookmark_css = 1;
-			}
-			$string = $article->text;
+				$string = $article->text;
 
-			$containerCount = 0;
-			$regex = "/{wookmark}(.*?){end-wookmark}/is";			
-			preg_match_all($regex, $string, $matches);
-			for ($n = 0; $n < count($matches[0]); $n++)
-			{
-				$arr_org = $matches[0][$n];
-				$arr_folder_path = $matches[1][$n]; // image path 
+				$containerCount = 0;
+				$regex = "/{wookmark}(.*?){end-wookmark}/is";			
+				preg_match_all($regex, $string, $matches);
+				for ($n = 0; $n < count($matches[0]); $n++)
+				{
+					$arr_org = $matches[0][$n];
+					$arr_folder_path = $matches[1][$n]; // image path 
 						
-				$singleRegex = "/{wookmark}(.*?){title}(.*?){align}(.*?){end-wookmark}/is";
-				preg_match_all($singleRegex, $arr_org, $singleMatches);
-				if (count($singleMatches[0]) > 0)
-				{
-					// only one match might happen...
-					$arr_image_path = $singleMatches[1][0];
-					$arr_image_title = $singleMatches[2][0];
-					$imageAlign = $singleMatches[3][0];
-					$returned = $this->fetchImgTxt($arr_image_path, $arr_image_title, $imageAlign);
-					$string= str_replace($arr_org, $returned, $string);
+					$singleRegex = "/{wookmark}(.*?){title}(.*?){align}(.*?){end-wookmark}/is";
+					preg_match_all($singleRegex, $arr_org, $singleMatches);
+					if (count($singleMatches[0]) > 0)
+					{
+						// only one match might happen...
+						$arr_image_path = $singleMatches[1][0];
+						$arr_image_title = $singleMatches[2][0];
+						$imageAlign = $singleMatches[3][0];
+						$returned = $this->fetchImgTxt($arr_image_path, $arr_image_title, $imageAlign);
+						$string= str_replace($arr_org, $returned, $string);
+					}
+					else
+					{
+						$insertDB = false;
+						// On peut avoir des paramètres du type "folder/folder|param1=value;param2=value;..."
+						// ici, $arr_folder_path vaut "folder/folder|param1=value;param2=value;..."
+						$parameters = explode("|", $arr_folder_path);
+						// On remet "folder/folder" dans $arr_folder_path
+						$arr_folder_path = $parameters[0];
+						if (count($parameters) > 1)
+						{
+							// lecture des paramètres
+							$parameters = explode(";", $parameters[1]);
+							if (count($parameters) > 0)
+							{
+								for ($paramIndex = 0; $paramIndex < count($parameters); $paramIndex++)
+								{
+									$pair = explode("=", $parameters[$paramIndex]);
+									if (count($pair) > 1)
+									{
+										$paramName = $pair[0];
+										if ($paramName == "insert")
+										{
+											$paramValue = $pair[1];
+											if ($paramValue == "y" || $paramValue == "Y")
+											{
+												$insertDB = true;
+											}
+										}
+									}
+								}
+							}
+						}
+						$returned = $this->fetchImgFold($arr_folder_path, $insertDB);
+						$string= str_replace($arr_org,"<div class='myapp' id='myapp$containerCount'><ul class='tiles' id='tiles$containerCount'>$returned</ul></div>",$string);
+						$containerCount++;
+					}
 				}
-				else
-				{
-					$returned = $this->fetchImgFold($arr_folder_path);
-					$string= str_replace($arr_org,"<div class='myapp' id='myapp$containerCount'><ul class='tiles' id='tiles$containerCount'>$returned</ul></div>",$string);
-					$containerCount++;
-				}
-			}
 
-			$javascript = "<script type='text/javascript'>
-			jQuery(document).imagesLoaded(function() {
-			jQuery(document).ready(new function() {";
+				$javascript = "<script type='text/javascript'>
+				jQuery(document).imagesLoaded(function() {
+					jQuery(document).ready(new function() {";
 			
-			for ($galleryIndex = 0; $galleryIndex < $containerCount; $galleryIndex++)
-			{
-				$javascript .= "
-				  var options$galleryIndex = {
-					autoResize: $autoresize_gal, 
-					container: jQuery('#myapp$galleryIndex'), 
-					offset: $offset, 
-					itemWidth: $itemWidth
-				  };
-				  var handler$galleryIndex = jQuery('#tiles$galleryIndex li');
-				  handler$galleryIndex.wookmark(options$galleryIndex);";
-			}
+					for ($galleryIndex = 0; $galleryIndex < $containerCount; $galleryIndex++)
+					{
+						$javascript .= "
+						var options$galleryIndex = {
+							autoResize: $autoresize_gal, 
+							container: jQuery('#myapp$galleryIndex'), 
+							offset: $offset, 
+							itemWidth: $itemWidth
+						};
+						var handler$galleryIndex = jQuery('#tiles$galleryIndex li');
+						handler$galleryIndex.wookmark(options$galleryIndex);";
+					}
 			  
-			$javascript .= "
-			  jQuery('.tiles li').tipsy({
-				gravity: 's',
-				html: true,
-				delayIn: 0,
-				delayOut: 0,
-				offset: 0,
-				opacity: 1,
-				fade: true,
-				title: function () {
-					var li = jQuery(this);
-					var a = li.children().first();
-					var title = a.attr('title');
-					if (title != null && title.length > 0)
-					{
-						li.attr('title', title);
-						a.attr('title', '');
-						return title;
+					$javascript .= "
+					jQuery('.tiles li').tipsy({
+						gravity: 's',
+						html: true,
+						delayIn: 0,
+						delayOut: 0,
+						offset: 0,
+						opacity: 1,
+						fade: true,
+						title: function () {
+							var li = jQuery(this);
+							var a = li.children().first();
+							var title = a.attr('title');
+							if (title != null && title.length > 0)
+							{
+								li.attr('title', title);
+								a.attr('title', '');
+								return title;
+							}
+							return li.attr('original-title');
+						}
+						});
+
+						});
+						});
+						</script>	";
+		  
+						$article->text = $string . $javascript;
+		  
 					}
-					return li.attr('original-title');
 				}
-			  });
 
-			});
-			});
-		  </script>	";
-		  
-		  $article->text = $string . $javascript;
-		  
-		}
-	}
-
-	function fetchImgFold($folder_path)
-	{
-		$base_url=JURI::root();
-		$tool = $this->params->def('tool');
-		$base=trim(JURI::base(true),'/\\');
-		$th_img_width = $this->params->def('th_img_width');
-		$folder_path=trim($folder_path,'\\/').'/';
-		$dir = opendir($folder_path);
-		while ($file = readdir($dir))
-		{ 
-		   if (preg_match('/(.*?)\.gif/i',$file) || preg_match('/(.*?)\.png/i',$file) || preg_match('/(.*?)\.jpg/i',$file) || preg_match('/(.*?)\.jpeg/i',$file) )
-		   { 
-			   $string1[] = $file;
-		   }
-		}
-		
-		// Get image caption
-		$fileInfo = $this->getFileInfo($folder_path);
-		
-		$tag='';
-		for($c = 0; $c < count($string1); $c++)
-		{
-			$fileName = $string1[$c];
-			$caption = str_replace("'", "&apos;", $fileInfo[$fileName]);
-			
-			$tag.="<li>
-					<a rel='shadowbox[gallery]' href='$base_url$folder_path$string1[$c]' title='$caption'>
-						<img src='$base_url".'plugins/content/wookmark_gallery/wookmark_gallery/tmpl/'."$tool.php?src=$base/$folder_path$string1[$c]&w=$th_img_width&q=100'>
-					</a>
-			</li>";
-		
-		}
-		return $tag;
-	}
-	
-    private function getFileInfo($imageFolder)
-    {
-		$file_info = false;
-
-		$db =& JFactory::getDBO();
-		$query = 'SELECT title, filename, filepath FROM `#__phocagallery` p WHERE p.filepath ="'. trim(substr($imageFolder, 7), '/') .'"';
-		$db->setQuery($query);
-		$data = $db->loadObjectList(); 
-		if (count($data))
-		{
-			foreach ($data as $row)
-			{
-				$file_info[$row->filename] = $row->title;
-			}
-		}
-		else
-		{
-			$absolute_path = JPATH_SITE;		
-			$captions_txtfile = $absolute_path.'/'.$imageFolder.'captions.txt';
-
-			if (file_exists($captions_txtfile))
-			{
-				$captions_file = array_map('trim', file($captions_txtfile));
-
-				foreach($captions_file as $value)
+				function readIptcData($file, $folder_path, &$imgData)
 				{
-					if(!empty($value))
+					$imgData[$file] = array();
+					$size = getimagesize($folder_path.'/'.$file, $info);
+					if(isset($info['APP13']))
 					{
-						$captions_line = explode('|', $value);
-						$file_info[$captions_line[0]] = $captions_line[1];
+					    $iptc = iptcparse($info['APP13']);
+						if (isset($iptc['2#005']))
+						{
+							$imgData[$file]['title'] = $iptc['2#005'][0];
+						}	
+						if (isset($iptc['2#025']))
+						{
+							$imgData[$file]['keywords'] = implode('|', $iptc['2#025']);
+						}	
+					}					
+				}
+
+				function fetchImgFold($folder_path, $insertInDatabase)
+				{
+					$base_url=JURI::root();
+					$tool = $this->params->def('tool');
+					$base=trim(JURI::base(true),'/\\');
+					$th_img_width = $this->params->def('th_img_width');
+					$folder_path=trim($folder_path,'\\/').'/';
+					$imgData = array();
+					$dir = opendir($folder_path);
+					while ($file = readdir($dir))
+					{ 
+						if (preg_match('/(.*?)\.gif/i',$file) || preg_match('/(.*?)\.png/i',$file) || preg_match('/(.*?)\.jpg/i',$file) || preg_match('/(.*?)\.jpeg/i',$file) )
+						{ 
+							$string1[] = $file;
+							if ($insertInDatabase)
+							{
+			   					// Read IPTC info from file (title + key words)
+								$this->readIptcData($file, $folder_path, $imgData);
+							}
+						}
 					}
+		
+					if ($insertInDatabase)
+					{
+						$this->updateImageTitles($folder_path, $imgData);
+					}
+					
+					// Get image caption
+					$fileInfo = $this->getFileInfo($folder_path);
+		
+					$tag='';
+					for($c = 0; $c < count($string1); $c++)
+					{
+						$fileName = $string1[$c];
+						$caption = str_replace("'", "&apos;", $fileInfo[$fileName]);
+			
+						$tag.="<li>
+							<a rel='shadowbox[gallery]' href='$base_url$folder_path$string1[$c]' title='$caption'>
+						<img src='$base_url".'plugins/content/wookmark_gallery/wookmark_gallery/tmpl/'."$tool.php?src=$base/$folder_path$string1[$c]&w=$th_img_width&q=100'>
+						</a>
+						</li>";
+		
+					}
+					return $tag;
+				}
+	
+				private function updateImageTitles($imageFolder, $imgData)
+				{
+					$filePath = trim(substr($imageFolder, 7), '/');
+					
+					// Remove all entries for the current folder
+					$db =& JFactory::getDBO();
+					$DeleteQquery = 'DELETE FROM `#__phocagallery` WHERE filepath ="'. $filePath .'"';
+					$db->setQuery($DeleteQquery);
+					$db->query();
+					
+					// Prepare insert query
+					$insertQuery = $db->getQuery(true);
+					
+					// Specify columns
+					$columns = array('title','filename','filepath','metadata','published');
+					
+					// IPopulate values array
+					$values = array();
+					foreach ($imgData as $fileName => $data)
+					{
+						$values[] = '"' . $data['title'] . '","' . $fileName . '","' . $filePath . '","' . $data['keywords'] . '",1';
+					}
+					$insertQuery->insert($db->quoteName('#__phocagallery'));
+					$insertQuery->columns($columns);
+					$insertQuery->values($values);
+					
+					// Execute query
+					$db->setQuery($insertQuery);
+					$db->query();
+				}
+
+				private function getFileInfo($imageFolder)
+				{
+					$file_info = false;
+
+					$db =& JFactory::getDBO();
+					$query = 'SELECT title, filename, filepath FROM `#__phocagallery` p WHERE p.filepath ="'. trim(substr($imageFolder, 7), '/') .'"';
+					$db->setQuery($query);
+					$data = $db->loadObjectList(); 
+					if (count($data))
+					{
+						foreach ($data as $row)
+						{
+							$file_info[$row->filename] = $row->title;
+						}
+					}
+					else
+					{
+						$absolute_path = JPATH_SITE;		
+						$captions_txtfile = $absolute_path.'/'.$imageFolder.'captions.txt';
+
+						if (file_exists($captions_txtfile))
+						{
+							$captions_file = array_map('trim', file($captions_txtfile));
+
+							foreach($captions_file as $value)
+							{
+								if(!empty($value))
+								{
+									$captions_line = explode('|', $value);
+									$file_info[$captions_line[0]] = $captions_line[1];
+								}
+							}
+						}
+					}
+
+					return $file_info;
+				}
+
+				function fetchImgTxt($arr_img_path, $arr_title, $align)
+				{
+					$base_url=JURI::root();
+					$tool = $this->params->def('tool');
+					$base=trim(JURI::base(true),'/\\');
+					$th_img_width = $this->params->def('th_img_width');
+					$img_path=explode(',',$arr_img_path);
+					$title=explode(',',$arr_title);
+					$wookmarkPath = $base_url . "plugins/content/wookmark_gallery/wookmark_gallery/tmpl/" . $tool . ".php";
+					$tag='';
+					for ($i = 0; $i < count($img_path); $i++)
+					{
+						$tag .= "<span class='wookmark-single-" . $align . "'>" .
+							"<a rel='shadowbox[single]' href='" . $base_url . $img_path[$i] . "' title='" . $title[$i] . "'>" .
+								"<img  src='" . $wookmarkPath . "?src=" . $base . "/" . $img_path[$i] . "&w=" . $th_img_width . "&q=100'>" .
+									"</a></span>";
+					}
+					return $tag;
 				}
 			}
-		}
-
-		return $file_info;
-    }
-
-	function fetchImgTxt($arr_img_path, $arr_title, $align)
-	{
-		$base_url=JURI::root();
-		$tool = $this->params->def('tool');
-		$base=trim(JURI::base(true),'/\\');
-		$th_img_width = $this->params->def('th_img_width');
-		$img_path=explode(',',$arr_img_path);
-		$title=explode(',',$arr_title);
-		$wookmarkPath = $base_url . "plugins/content/wookmark_gallery/wookmark_gallery/tmpl/" . $tool . ".php";
-		$tag='';
-		for ($i = 0; $i < count($img_path); $i++)
-		{
-			$tag .= "<span class='wookmark-single-" . $align . "'>" .
-					"<a rel='shadowbox[single]' href='" . $base_url . $img_path[$i] . "' title='" . $title[$i] . "'>" .
-					"<img  src='" . $wookmarkPath . "?src=" . $base . "/" . $img_path[$i] . "&w=" . $th_img_width . "&q=100'>" .
-					"</a></span>";
-		}
-		return $tag;
-	}
-}
-?>
+			?>
