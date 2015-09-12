356edeaf9099e72a276743e661ef3e0ad6348c71
davidguillemet <davidguillemet@iMac-27-DGUI.local>
2015-09-13 01:26:43 +0200

    google jsapi asynchron loading
    google jsapi asynchron loading

--- templates/vg_fashion/index.php
+++ templates/vg_fashion/index.php
@@ -93,7 +93,7 @@ if( $this->countModules('fashion-right-top') or $this->countModules('fashion-rig
 <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
-<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript" async src="https://www.google.com/jsapi"></script>
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,300,600,700,400&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,600&subset=latin-ext' rel='stylesheet' type='text/css'>
 <script type="text/javascript">
