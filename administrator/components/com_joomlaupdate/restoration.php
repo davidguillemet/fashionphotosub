d465e458d42985554a8eb493915523ddd5c7998a
davidguillemet <davidguillemet@iMac-27-DGUI.local>
2016-07-14 18:40:09 +0200

    Joomla 3.6.0
    Joomla 3.6.0

---
+++ administrator/components/com_joomlaupdate/restoration.php
@@ -0,0 +1,13 @@
+<?php
+defined('_AKEEBA_RESTORATION') or die('Restricted access');
+$restoration_setup = array(
+	'kickstart.security.password' => 'mF1qRD1GtETsJesZpfFov59Qx8i0Fee8',
+	'kickstart.tuning.max_exec_time' => '5',
+	'kickstart.tuning.run_time_bias' => '75',
+	'kickstart.tuning.min_exec_time' => '0',
+	'kickstart.procengine' => 'direct',
+	'kickstart.setup.sourcefile' => '/Applications/MAMP/htdocs/fashion/tmp/Joomla_3.6.0-Stable-Update_Package.zip',
+	'kickstart.setup.destdir' => '/Applications/MAMP/htdocs/fashion',
+	'kickstart.setup.restoreperms' => '0',
+	'kickstart.setup.filetype' => 'zip',
+	'kickstart.setup.dryrun' => '0');
